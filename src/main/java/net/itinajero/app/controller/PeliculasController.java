fa9ead60b20c84d37bcc230f02d06680278d2d30
Uriel Meléndez Sánchez <47426294+umeldz@users.noreply.github.com>
2019-05-13 22:55:09 -0500

    save files in the server

--- src/main/java/net/itinajero/app/controller/PeliculasController.java
+++ src/main/java/net/itinajero/app/controller/PeliculasController.java
@@ -1,8 +1,11 @@
 package net.itinajero.app.controller;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import net.itinajero.app.model.Pelicula;
@@ -41,26 +46,50 @@ public class PeliculasController {
 	@GetMapping("/create")
 	public String crear() {
 
-		
 		return "peliculas/formPelicula";
 	}
 
 	@PostMapping("/save")
-	public String guardar(Pelicula pelicula, BindingResult result, RedirectAttributes attributes) {
+	public String guardar(Pelicula pelicula, BindingResult result, RedirectAttributes attributes,
+			@RequestParam("archivoImagen") MultipartFile multipart, HttpServletRequest request) {
 
 		if (result.hasErrors()) {
 			System.out.println("existieron errores");
 			return "peliculas/formPelicula";
 		}
-		System.out.println("elementos de la lista antes: " + servicePelicula.buscarTodas().size());
+	
+		
+		if(!multipart.isEmpty()){
+			String nombreImagen = guardarImagen(multipart, request);
+			pelicula.setImagen(nombreImagen);
+		}
 		
 		servicePelicula.insertar(pelicula);
-		System.out.println("elementos de la lista despues de insertar: " + servicePelicula.buscarTodas().size());
-		System.out.println("Guardando pelicula" + pelicula);
 		
 		attributes.addFlashAttribute("mensaje","El registro fue guardado");
 		//return "peliculas/formPelicula";
 		return "redirect:/peliculas/index";
+	}
+	
+	private String guardarImagen(MultipartFile multipart, HttpServletRequest request){
+		//Se obtiene el nombre del archivo original
+		String nombreOriginal= multipart.getOriginalFilename();
+		//Se obtiene la ruta absoluta del directorio imagenes
+		// apache-tomcat/webapps/cineapp/resources/images
+		String rutaFinal= request.getServletContext().getRealPath("/resources/images/");
+		
+		try {
+			//Se forma el nombre del archivo para guardarlo
+			File imageFile= new File(rutaFinal + nombreOriginal);
+			//Se guarda fisicamente el archivo en el disco duro
+			multipart.transferTo(imageFile);
+			return nombreOriginal;
+			
+		} catch (Exception e) {
+			System.out.println("Error " + e.getMessage());
+			return null;
+		}
+
 	}
 
 	@InitBinder
